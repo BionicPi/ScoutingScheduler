9050aa45954bbbbfeb9dba0751cd40917b02f97e
BionicPi <mxmeinhold@gmail.com>
2016-03-16 20:56:01 -0400

    Working

--- Scouting/src/backEnd/Scheduler.java
+++ Scouting/src/backEnd/Scheduler.java
@@ -1,9 +1,17 @@
 package backEnd;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Scheduler 
 {
+	
+	final static String ls = System.getProperty("line.separator");
+
 	
 	public ArrayList<Match> matchList;
 	public ArrayList<Scouter> scouters;
@@ -31,7 +39,7 @@ public class Scheduler
 			blue2 = b2;
 			blue3 = b3;
 			red1 = r1;
-			red2 = r3;
+			red2 = r2;
 			red3 = r3;
 			
 			teams = new ArrayList<Team>();
@@ -70,10 +78,12 @@ public class Scheduler
 		
 		void filterMatches()
 		{
+			this.sortTeams();
 			for(Team t: teams)
 				for(Match m: matchList)
 					if(m.teams.contains(t))
 						matches.add(m);
+			this.sortMatches();
 		}
 		
 		void sortMatches()
@@ -84,14 +94,19 @@ public class Scheduler
 				if(!(temp.size() > 0))
 					temp.add(matches.get(i));
 				else
-					for(int j = 0; i < temp.size();)
+					for(int j = 0; j < temp.size();)
 						if(temp.get(j).getMatchNumber() > matches.get(i).getMatchNumber())
 						{
 							temp.add(j, matches.get(i));
-							i = temp.size();
+							j = temp.size();
 						}
+						else if(j < temp.size() - 1)
+							j++;
 						else
-							i++;
+						{
+							temp.add(matches.get(i));
+							j = temp.size();
+						}
 			
 			matches = temp;
 		}
@@ -104,16 +119,29 @@ public class Scheduler
 				if(!(temp.size() > 0))
 					temp.add(teams.get(i));
 				else
-					for(int j = 0; i < temp.size();)
+					for(int j = 0; j < temp.size();)
 						if(temp.get(j).getTeamNumber() > teams.get(i).getTeamNumber())
 						{
 							temp.add(j, teams.get(i));
-							i = temp.size();
+							j = temp.size();
 						}
+						else if(j < temp.size() - 1)
+							j++;
 						else
-							i++;
+						{
+							temp.add(teams.get(i));
+							j = temp.size();
+						}
 			
 			teams = temp;
+		}
+
+		public String getMatchNumbers() 
+		{
+			String s = "";
+			for(Match m: matches)
+				s += m.getMatchNumber() + " ";
+			return s;
 		}
 	}
 	
@@ -140,25 +168,25 @@ public class Scheduler
 			return number;
 		}
 		
-		void sortMatches()
-		{
-			ArrayList<Match> temp = new ArrayList<Match>();
-			
-			for(int i = 0; i < matches.size(); i++)
-				if(!(temp.size() > 0))
-					temp.add(matches.get(i));
-				else
-					for(int j = 0; i < temp.size();)
-						if(temp.get(j).getMatchNumber() > matches.get(i).getMatchNumber())
-						{
-							temp.add(j, matches.get(i));
-							i = temp.size();
-						}
-						else
-							i++;
-			
-			matches = temp;
-		}
+//		void sortMatches()
+//		{
+//			ArrayList<Match> temp = new ArrayList<Match>();
+//			
+//			for(int i = 0; i < matches.size(); i++)
+//				if(!(temp.size() > 0))
+//					temp.add(matches.get(i));
+//				else
+//					for(int j = 0; i < temp.size();)
+//						if(temp.get(j).getMatchNumber() > matches.get(i).getMatchNumber())
+//						{
+//							temp.add(j, matches.get(i));
+//							i = temp.size();
+//						}
+//						else
+//							i++;
+//			
+//			matches = temp;
+//		}
 		
 		public String toString()
 		{
@@ -170,15 +198,15 @@ public class Scheduler
 	{
 		boolean intersect = false;
 		
-		for(int i = 0; i < teamList.size() - 1; i++)
-			for(int j = i + 1; j<teamList.size(); j++)
-			{
-				for(Match m: teamList.get(i).matches)
-					if(teamList.get(j).matches.contains(m))
+		for(Team t: teamList)
+			for(Match m: t.matches)
+				for(Team ot: teamList)
+				{
+					if(ot.matches.contains(m))
 						intersect = true;
-				if(!intersect)
-					System.out.println(teamList.get(i).getTeamNumber() + " " + teamList.get(j).getTeamNumber());
-			}				
+					if(!intersect)
+						System.out.println(t.getTeamNumber() + " " + ot.getTeamNumber());
+				}
 	}
 	
 	public void parseTeamList(String list)
@@ -194,24 +222,29 @@ public class Scheduler
 	
 	public void parseMatchList(String list)
 	{		
+		int j = 0;
+//		System.out.println(list.replaceAll(ls, ","));
+		list = list.replaceAll(ls, ",");
 		for(int i = 0; i < list.length();)
 		{
-			int number = Integer.parseInt(list.substring(i, list.indexOf(" ", i)));
-			i = list.indexOf(" ", i) + 1;
-			Team b1 = new Team(Integer.parseInt(list.substring(i, list.indexOf(" ", i))));
-			i = list.indexOf(" ", i) + 1;
-			Team b2 = new Team(Integer.parseInt(list.substring(i, list.indexOf(" ", i))));
-			i = list.indexOf(" ", i) + 1;
-			Team b3 = new Team(Integer.parseInt(list.substring(i, list.indexOf(" ", i))));
-			i = list.indexOf(" ", i) + 1;
-			Team r1 = new Team(Integer.parseInt(list.substring(i, list.indexOf(" ", i))));
-			i = list.indexOf(" ", i) + 1;
-			Team r2 = new Team(Integer.parseInt(list.substring(i, list.indexOf(" ", i))));
-			i = list.indexOf(" ", i) + 1;
-			Team r3 = new Team(Integer.parseInt(list.substring(i, list.indexOf(" ", i))));
-			i = list.indexOf(" ", i) + 1;
+			Team b1 = new Team(Integer.parseInt(list.substring(i, list.indexOf(",", i))));
+			i = list.indexOf(",", i) + 1;
+			Team b2 = new Team(Integer.parseInt(list.substring(i, list.indexOf(",", i))));
+			i = list.indexOf(",", i) + 1;
+			Team b3 = new Team(Integer.parseInt(list.substring(i, list.indexOf(",", i))));
+			i = list.indexOf(",", i) + 1;
+			Team r1 = new Team(Integer.parseInt(list.substring(i, list.indexOf(",", i))));
+			i = list.indexOf(",", i) + 1;
+			Team r2 = new Team(Integer.parseInt(list.substring(i, list.indexOf(",", i))));
+			i = list.indexOf(",", i) + 1;
+			Team r3 = new Team(Integer.parseInt(list.substring(i, list.indexOf(",", i))));
+			i = list.indexOf(",", i) + 1;
 			
-			matchList.add(new Match(number, b1, b2, b3, r1, r2, r3));
+//			System.out.println(b1.getTeamNumber() + " " + b2.getTeamNumber() + " " + b3.getTeamNumber() + " " + r1.getTeamNumber() + " " + r2.getTeamNumber() + " " + r3.getTeamNumber() + "");
+			
+			j++;
+			
+			matchList.add(new Match(j, b1, b2, b3, r1, r2, r3));
 		}
 		
 		for(Match m: matchList)
@@ -223,25 +256,36 @@ public class Scheduler
 			t.filterMatches();
 	}
 	
-	void sortMatchList()
+	public void clearScouterOverlap()
 	{
-		ArrayList<Match> temp = new ArrayList<Match>();
-		
-		for(int i = 0; i < matchList.size(); i++)
-			if(!(temp.size() > 0))
-				temp.add(matchList.get(i));
-			else
-				for(int j = 0; i < temp.size();)
-					if(temp.get(j).getMatchNumber() > matchList.get(i).getMatchNumber())
-					{
-						temp.add(j, matchList.get(i));
-						i = temp.size();
-					}
-					else
-						i++;
-		
-		matchList = temp;
+		for(int i = 0; i < 5 * scouters.size(); i++)
+		{
+			scouters.get(i % scouters.size()).sortMatches();
+			for(int j = 0; j < scouters.get(i % scouters.size()).matches.size()-1; j++)
+				if(scouters.get(i % scouters.size()).matches.get(j).getMatchNumber() == scouters.get(i % scouters.size()).matches.get(j + 1).getMatchNumber())
+					scouters.get((i+1) % scouters.size()).matches.add(scouters.get(i % scouters.size()).matches.remove(j + 1));
+		}
 	}
+	
+//	void sortMatchList()
+//	{
+//		ArrayList<Match> temp = new ArrayList<Match>();
+//		
+//		for(int i = 0; i < matchList.size(); i++)
+//			if(!(temp.size() > 0))
+//				temp.add(matchList.get(i));
+//			else
+//				for(int j = 0; i < temp.size();)
+//					if(temp.get(j).getMatchNumber() > matchList.get(i).getMatchNumber())
+//					{
+//						temp.add(j, matchList.get(i));
+//						i = temp.size();
+//					}
+//					else
+//						i++;
+//		
+//		matchList = temp;
+//	}
 	
 	void sortTeamList()
 	{
@@ -268,21 +312,81 @@ public class Scheduler
 		teamList = temp;
 	}
 	
+	public void addScouter(String s)
+	{
+		this.scouters.add(new Scouter(s));
+	}
+	
 	
 	
 	public static void main(String [] args)
 	{
 		Scheduler VAHAY = new Scheduler();
 		
-		VAHAY.parseMatchList("1 539 6236 5549 619 2068 612 2 888 614 3274 3373 5587 1123 3 2186 5338 2363 1719 5243 2911 4 4472 4505 4242 6189 3872 611 ");
-		VAHAY.sortMatchList();
-		VAHAY.sortTeamList();
-		
-		for(Team t: VAHAY.teamList)
-		{
-			System.out.println(t.getTeamNumber());
-			for(Match m: t.matches)
-				System.out.println(m.getMatchNumber());
+		try {
+			String line = null;
+			StringBuilder stringBuilder = new StringBuilder();
+
+			FileReader file = new FileReader("MatchSchedule.csv");
+			BufferedReader bFile = new BufferedReader(file);
+			
+			while((line = bFile.readLine()) != null)
+			{
+				stringBuilder.append(line);
+				stringBuilder.append(ls);
+			}
+//			System.out.println(stringBuilder.toString());
+
+			bFile.close();
+			file.close();
+			
+			
+
+			VAHAY.parseMatchList(stringBuilder.toString());
+//			VAHAY.sortMatchList();
+			VAHAY.sortTeamList();
+//			System.out.println(VAHAY.matchList);
+			
+			VAHAY.coincidence();
+			
+			VAHAY.addScouter("Megan/Alex");
+			VAHAY.addScouter("Emily");
+			VAHAY.addScouter("Alyssa");
+			VAHAY.addScouter("Katie");
+			VAHAY.addScouter("Maddie");
+			VAHAY.addScouter("Ramona");
+			VAHAY.addScouter("John");
+			VAHAY.addScouter("Sean");
+			VAHAY.addScouter("Brendan");
+			VAHAY.addScouter("Celia");
+			VAHAY.addScouter("Daniel/Ricardo");
+			VAHAY.addScouter("Braidan/Max");
+			VAHAY.addScouter("Mary/Marielle");
+			VAHAY.addScouter("Hannah/Brittany");
+			
+
+			for(int j = 0; j < VAHAY.teamList.size(); j++)
+				if(VAHAY.teamList.get(j).getTeamNumber() != 1111)
+					VAHAY.scouters.get(j%VAHAY.scouters.size()).teams.add(VAHAY.teamList.get(j));
+			
+
+			
+			for(Scouter s: VAHAY.scouters)
+				s.filterMatches();
+
+			VAHAY.clearScouterOverlap();
+			
+			for(Scouter s: VAHAY.scouters)
+				System.out.println(s.name + ls + s.getMatchNumbers());
+
+//			for(Team t: VAHAY.teamList)
+//			{
+//				System.out.println(t.getTeamNumber());
+//				for(Match m: t.matches)
+//					System.out.println(m.getMatchNumber());
+//			}
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 	}
 }
